636d6dba425f8b11b641f605c4835e5e46d402af
natan <natan.spricigo@camtwo.com.br>
2017-10-03 11:36:53 -0300

    master [testes heroku]

--- app.js
+++ app.js
@@ -4,7 +4,29 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-server.use(router)
+
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+    console.log('acho');
+    res.jsonp(req.rawHeaders);
+});
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+    if (req.method === 'POST') {
+        req.body.createdAt = Date.now()
+    }
+    // Continue to JSON Server router
+    next();
+});
+
+server.use(router);
+if (process.env.PORT == undefined) {
+    process.env.PORT = 3000;
+}
+
 server.listen(process.env.PORT, () => {
-    console.log('JSON Server is running')
+    console.log('JSON Server esta vivoooo !!! na porta ' + process.env.PORT);
 });
